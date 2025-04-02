9eb214e9372fcc2dc3f6c772399d796e16e773a2
tutto2 <tuto.2@hotmail.com>
2025-04-02 13:39:36 -0500

    Initialize and Set up React frontend

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/prop-types": "off",
+  },
+};
