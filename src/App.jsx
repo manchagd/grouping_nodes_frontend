fda2c031b009618601799aa5a4d3acfe5d38092d
Carlos Augusto Herrera Orozco <carlosh36@gmail.com>
2025-04-06 14:10:53 -0500

    Initialize react frontend project (#1)
    * Initialize React frontend using Vite
    
    * React and Tailwind set up
    
    * Resolving several bugs on initial configuration of Tailwind
    
    * Tailwind tested and working
    
    * Deleted needless files
    ---------
    
    Co-authored-by: Daniel GD <mancha0703@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg">
+      Tailwind is working!
+    </div>
+  );
+}
+
+export default App
