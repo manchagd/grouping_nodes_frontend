2cbfe9591a580dd33b42d78eeb0fc6a9464485da
tutto2 <tuto.2@hotmail.com>
2025-04-23 17:02:28 -0500

    Axios configured, new component "DatetimeViewer", AppRouter updated and connection to API tested and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const client = axios.create({
-    baseURL: 'http://localhost:3000'
-  })
-
   return (
     <div className="bg-blue-500 text-white p-4 rounded-lg">
       Tailwind is working!
---
+++ src/components/DatetimeViewer.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import api from '../api/axios';
+
+function DatetimeViewer() {
+  const [datetime, setDatetime] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDatetime = async () => {
+      try {
+        const response = await api.get('/current_datetime');
+        setDatetime(response.data.datetime);
+      } catch (err) {
+        console.error('Unable to obtain date and time:', err);
+        setError('Couldnt found date and time');
+      }
+    };
+
+    fetchDatetime();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2 text-gray-800">Date and Time from Rails:</h1>
+      {error && <p className="text-red-500 font-bold">{error}</p>}
+      {datetime ? (
+        <p className="text-gray-500 font-bold">{datetime}</p>
+      ) : (
+        !error && <p className="text-gray-400">Charging...</p>
+      )}
+    </div>
+  );
+}
+
+export default DatetimeViewer;
