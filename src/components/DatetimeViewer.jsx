93e967f75084149b692225baddab559de68aeef8
Carlos Augusto Herrera Orozco <carlosh36@gmail.com>
2025-04-30 12:53:03 -0500

    Frontend and Backend connection
    * Axios installed and configurated
    
    * Axios configured, new component "DatetimeViewer", AppRouter updated and connection to API tested and working
    
    * Added link to datetime
    
    * Update the name of the previous component to make it more descriptive.
    
    * Delete console log for error
    
    * Updated component name to follow RESTful principles, AppRoutes and MainLayout updated as well. Removed console logs for cleaner error handling.

---
+++ src/components/DatetimeViewer.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import api from '../api/axios';
+
+function DatetimeViewer() {
+  const [datetime, setDatetime] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDatetime = async () => {
+      try {
+        const response = await api.get('/datetime');
+        setDatetime(response.data.current_time);
+      } catch (err) {
+        setError('Couldnt found date and time');
+      }
+    };
+
+    fetchDatetime();
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2 text-gray-800">Date and Time from Rails:</h1>
+      {error && <p className="text-red-500 font-bold">{error}</p>}
+      {datetime ? (
+        <p className="text-gray-500 font-bold">{datetime}</p>
+      ) : (
+        !error && <p className="text-gray-400">Charging...</p>
+      )}
+    </div>
+  );
+}
+
+export default DatetimeViewer;
